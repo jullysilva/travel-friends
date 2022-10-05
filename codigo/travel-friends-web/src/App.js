9d3e67c0594f33e304604d6f06ef46d0878bef0a
Lorrayne Reis <loohsilva995@gmail.com>
2022-10-05 12:29:06 -0300

    login,recuperar conta, registrar

---
+++ codigo/travel-friends-web/src/App.js
@@ -0,0 +1,22 @@
+import Login   from "./components/Login.jsx";
+import Recuperar from "./components/RecuperarConta.jsx";
+import Registrar from "./components/Registrar.jsx"
+
+function App() {
+  return (
+
+    <>
+    
+    <Login/>
+    <Recuperar/>
+    <Registrar/>
+  
+
+    </>
+
+      
+
+  );
+}
+
+export default App;
