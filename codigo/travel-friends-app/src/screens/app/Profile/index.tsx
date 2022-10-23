d705c197798a1071cc62b9c898fb2982e7b05ed2
WarleyLeandro <warley.leandro95@gmail.com>
2022-10-23 19:58:01 -0300

    finish profile screen

--- codigo/travel-friends-app/src/screens/app/Profile/index.tsx
+++ codigo/travel-friends-app/src/screens/app/Profile/index.tsx
@@ -1,20 +1,72 @@
+import { FontAwesome } from "@expo/vector-icons";
 import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, StatusBar } from "react-native";
+import TextApp from "../../../components/Text";
+import { formatNickName } from "../../../utils/formatNickName";
 import theme from "../../../utils/theme";
-import { style } from "./styles";
+import { styles } from "./styles";
 
 export function Profile() {
   return (
-    <View style={style.container}>
-      <View style={style.logo}>
-        <Text
-          style={[
-            style.title,
-            { color: theme.colors.title, fontWeight: "600" },
-          ]}
-        >
-          Perfil
-        </Text>
+    <View style={styles.container}>
+      <View style={styles.headerProfile}>
+        <View style={styles.nickName}>
+          <TextApp
+            text={formatNickName("Leo Anjos")}
+            size={24}
+            color={theme.colors.shape}
+          />
+        </View>
+        <View style={styles.underlineProfile}>
+          <TextApp text="Leo Anjos" size={24} />
+        </View>
+      </View>
+      <View style={styles.bodyProfile}>
+        <View style={styles.menuProfile}>
+          <TouchableOpacity>
+            <View style={styles.underlineButton}>
+              <FontAwesome
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                //@ts-ignore
+                name={"heart"}
+                size={24}
+                color={theme.colors.title}
+                style={{ marginRight: 20 }}
+              />
+              <TextApp text="Favoritos" size={theme.fonts.text} />
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <View style={styles.underlineButton}>
+              <FontAwesome
+                name={"user"}
+                size={24}
+                color={theme.colors.title}
+                style={{ marginRight: 20 }}
+              />
+              <TextApp text="Editar conta" size={theme.fonts.text} />
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <View style={styles.underlineButton}>
+              <FontAwesome
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                //@ts-ignore
+                name={"arrow-circle-o-right"}
+                size={24}
+                color={theme.colors.title}
+                style={{ marginRight: 20 }}
+              />
+              <TextApp
+                text="sair"
+                size={theme.fonts.text}
+                color={theme.colors.title}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
